5318d39c530d0ac9aee3f5813e61c64bd4c20e07
spex2024 <dercolbags@gmail.com>
2024-05-24 20:21:00 +0000

    initial push

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -58,6 +58,9 @@ function Footer(props) {
                 </div>
                 <div className="flex items-center mt-5 sm:mt-0">
                     <ul className="flex items-center gap-5 sm:gap-10 text-slate-600 font-semibold font-serif">
+                        <Link to={'/dashboard'}>
+                            <li className="list-none">Dashboard</li>
+                        </Link>
                         <Link to={'/'}>
                             <li className="list-none">Terms</li>
                         </Link>
