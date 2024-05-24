3ff1c40ca8be8e8b1a06559f3d723f62e6d65d86
spex2024 <dercolbags@gmail.com>
2024-05-24 20:29:00 +0000

    initial push

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors({
-    origin: 'https://spex-waitlist.vercel.app',
+    origin: 'https://spex-africa.onrender.com',
     credentials :true
 
 }));
