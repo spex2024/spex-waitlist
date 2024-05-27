a6a163b8ae2cf785ed2c26fe203a9d85fa892e72
spex2024 <dercolbags@gmail.com>
2024-05-27 12:38:11 +0000

    initial push

--- frontend/src/components/Feedback.jsx
+++ frontend/src/components/Feedback.jsx
@@ -80,7 +80,7 @@ const Feedback = () => {
 
             {selectedItem && (
                 <div className="fixed inset-0 bg-gray-600 bg-opacity-75 flex items-center justify-center z-50 cursor-pointer px-10">
-                    <div className="flex flex-col items-center gap-5 bg-white rounded-lg shadow-lg p-6 w-11/12 md:w-2/3 lg:w-[60vw] lg:h-[90vh] py-10 overflow-y-auto">
+                    <div className="flex flex-col items-start justify-center gap-5 bg-white rounded-lg shadow-lg p-6 w-11/12 md:w-2/3 lg:w-[60vw] lg:h-[90vh] py-10 overflow-y-auto">
                         <h2 className="text-2xl font-bold mb-4">Feedback Details</h2>
                         {modalDetails.map((detail, index) => (
                             <div key={index} className={` w-full bg-amber-200 items-center justify-start flex flex-col gap-5`}>
